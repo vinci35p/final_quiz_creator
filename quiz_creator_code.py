5b39f90e3057c61c9437bf533012cef5b1420355
Vincent35p <vinsoyt4321@gmail.com>
2025-04-08 00:14:25 +0800

    edited list to work, return values in def func, used dict in choices and edited append (now it works)

--- quiz_creator_code.py
+++ quiz_creator_code.py
@@ -1,17 +1,28 @@
+# List of inputted values
+questions_list = []
+choices_list = []
+answers_list = []
+
 # Input question
 def question():
     user_question = str(input("Enter your preferred question: "))
+    return user_question
 
 # Input choices
 def choices():
-    user_choice_a = str(input("Enter choice a.: "))
-    user_choice_b = str(input("Enter choice b.: "))
-    user_choice_c = str(input("Enter choice c.: "))
-    user_choice_d = str(input("Enter choice d.: "))
+    user_choice = {
+        "A": input("Enter choice A: "),
+        "B": input("Enter choice B: "),
+        "C": input("Enter choice C: "),
+        "D": input("Enter choice D: ")
+    }
+    return user_choice
 
 # Input correct answer
 def answer():
-    user_ans = str(input("Enter the correct answer letter from provided question: "))
+    user_ans = str(input("Enter the correct answer (A,B,C,D) from provided question: ")).upper()
+
+    return user_ans
 
 # Loop until exit
 while True:
@@ -19,9 +30,13 @@ while True:
     choice = int(input("Enter '7' to exit. Enter '1' to continue: "))
 
     if choice == 1:
-        question()
-        choices()
-        answer()
+        q = question()
+        c = choices()
+        a = answer()
+
+        questions_list.append(a)
+        choices_list.append(c)
+        answers_list.append(a)
 
     elif choice == 7:
         print("Exiting, have a nice day!")
@@ -29,3 +44,5 @@ while True:
 
     else:
         print("Invalid input, enter just '1' or '7'.\n")
+
+print(questions_list)
