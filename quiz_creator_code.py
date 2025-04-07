be1c88f24601be52758e4e982101a01d0cfdb168
Vincent35p <vinsoyt4321@gmail.com>
2025-04-07 23:25:02 +0800

    created pseudocode for quiz creator, and added def functions for each needed input

---
+++ quiz_creator_code.py
@@ -0,0 +1,16 @@
+# Input question
+def question():
+    user_question = str(input("Enter your preferred question: "))
+
+# Input choices
+def choices():
+    user_choice_a = str(input("Enter choice a.: "))
+    user_choice_b = str(input("Enter choice b.: "))
+    user_choice_c = str(input("Enter choice c.: "))
+    user_choice_d = str(input("Enter choice d.: "))
+
+# Input correct answer
+def answer():
+    user_ans = str(input("Enter the correct answer letter from provided question: "))
+
+# Loop until exit
