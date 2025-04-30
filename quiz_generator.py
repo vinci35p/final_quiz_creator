25b62df7ce4c0037a0c976ab279055b28201de44
Vincent35p <vinsoyt4321@gmail.com>
2025-04-30 22:07:11 +0800

    added colors and spaces to make it more pleasing to the eyes or cooler

--- quiz_generator.py
+++ quiz_generator.py
@@ -124,13 +124,13 @@ def execute_quiz(question_list):
                 print("Invalid input. Please enter a viable answer (A, B, C, or D).")
 
         if user_ans == question_list["answer"]:
-            print("You got it!")
+            print(Fore.GREEN + "You got it!")
             score += 1
 
         else:
-            print("Didn't got it, keep up!")
+            print(Fore.LIGHTRED_EX + "Didn't got it, keep up!")
 
-    print(f"\nYou got {score} out of {total}.\n")
+    print(Fore.LIGHTYELLOW_EX + f"\nYou got {score} out of {total}.\n")
 
 def delete_quiz_file(quiz_txt):
     try:
@@ -145,16 +145,16 @@ file = 'quiz_txt'
 quiz_questions = read_txt_file(file)
 
 while True:
-    print("Welcome to your own quiz creator!\n")
+    print(Fore.CYAN + "Welcome to your own quiz creator!\n")
     while True:
-        user_decision = str(input("Enter '1' if you want to create a new set of questionnaires, '2' if you just want "
+        user_decision = str(input(Fore.LIGHTBLUE_EX + "Enter '1' if you want to create a new set of questionnaires, '2' if you just want "
                               "to add questions, and '3' if you don't want to continue: "))
 
         if user_decision in ['1', '2', '3']:
             break
 
         else:
-            print("Invalid input. Enter just '1', '2' or '3'.")
+            print(Fore.RED + "Invalid input. Enter just '1', '2' or '3'.")
 
     if user_decision == '1':
         delete_quiz_file(file)
@@ -167,33 +167,34 @@ while True:
         break
 
 while True:
-    print("Do you like to take the quiz now?")
+    print(Fore.YELLOW + "\nDo you like to take the quiz now?")
 
     while True:
-        user_choice = str(input("Enter your choice (Y/N): ")).strip().upper()
+        user_choice = str(input(Fore.LIGHTRED_EX + "Enter your choice (Y/N): ")).strip().upper()
 
         if user_choice in ['Y','N']:
             break
         else:
-            print("Invalid input, enter 'Y' or 'N'.")
+            print(Fore.RED + "Invalid input, enter 'Y' or 'N'.")
 
     if user_choice == 'Y':
         quiz_questions = read_txt_file(file)
         if not quiz_questions:
-            print("No quiz questions found. Please create questions first.")
+            print(Fore.MAGENTA + "No quiz questions found. Please create questions first.")
             break
         else:
             retry = 'Y'
             while retry == 'Y':
                 execute_quiz(quiz_questions)
                 while True:
-                    retry = str(input("Do you want to take the quiz again? (Y/N): "))
+                    retry = str(input(Fore.GREEN + "Do you want to take the quiz again? (Y/N): "))
                     if retry in ['Y', 'N']:
                         break
                     else:
-                        print("Invalid input. Enter 'Y' or 'N'.")
-            print("Returning to main query.")
+                        print(Fore.RED + "Invalid input. Enter 'Y' or 'N'.")
+            print(Fore.GREEN + "Returning to main query.")
 
 
     if user_choice == 'N':
+        print(Fore.LIGHTGREEN_EX + "\nExiting, have a nice day!")
         break
