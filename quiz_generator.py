17f06d8c5944a50921e0f4dc848650106f53b36c
Vincent35p <vinsoyt4321@gmail.com>
2025-04-30 16:48:01 +0800

    created a new def function consisting the quiz compiler to use it in a loop where user can choose after quiz if they want to input or add new questions

--- quiz_generator.py
+++ quiz_generator.py
@@ -3,52 +3,6 @@ import sys
 import time
 import random
 
-# Text file to collect and write the data
-file = open("quiz_txt.txt", "a")
-
-# Question number, and proceeding
-def starting_num():
-    try:
-        with open("collected_data.txt", "r") as f:
-            lines = f.readlines()
-            return sum(1 for line in lines if line.startswith("Question: " or line[0].isdigit()))
-    except FileNotFoundError:
-        return 0
-
-question_num = starting_num() + 1
-
-# Input question
-def question():
-    user_question = str(input(Fore.CYAN + "\nEnter your preferred question: "))
-
-    return user_question
-
-# Input choices
-def quest_choices():
-    choices = {}
-    for letter in ['A', 'B', 'C', 'D']:
-        choices[letter] = input(Fore.BLUE+ f"Enter choice {letter} : ")
-
-    return choices
-
-# Input correct answer
-def answer():
-    user_ans = str(input(Fore.LIGHTBLUE_EX + "\nEnter the correct answer (A,B,C,D) from provided question: ")).upper()
-    while user_ans not in ["A", "B", "C", "D"]:
-        user_ans = input(Fore.RED + "Invalid input. Please enter A, B, C, or D: ").upper()
-
-    return user_ans
-
-# Loading text after questions, choices and answers are inputted
-def loading_animation():
-    print(Fore.LIGHTRED_EX + "Saving your question", end='')
-    for _ in range(3):
-        sys.stdout.write(Fore.RED + ".")
-        sys.stdout.flush()
-        time.sleep(0.75)
-    print(Fore.YELLOW + "\nSaved successfully! Check your text file to see your inputted values.")
-    print()
-
 # Read quiz file and randomly select question with choices
 def read_txt_file(quiz_txt):
     with open("quiz_txt.txt", "r") as f:
@@ -101,37 +55,87 @@ def execute_quiz(question_list):
 
 
 # Main loop until exit
-while True:
-    print(Fore.LIGHTYELLOW_EX + "Enter your question!")
-    while True:
-        choice = str(input(Fore.YELLOW + "Enter '7' to exit. Enter '1' to continue: "))
-        if choice in ['1', '7']:
-            break
+def compiler_quiz():
+    # Text file to collect and write the data
+    file = open("quiz_txt.txt", "a")
 
-        else:
-            print(Fore.RED + "Invalid input. Enter just number '1' to continue, and '7' to exit\n")
+    # Question number, and proceeding
+    def starting_num():
+        try:
+            with open("collected_data.txt", "r") as f:
+                lines = f.readlines()
+                return sum(1 for line in lines if line.startswith("Question: " or line[0].isdigit()))
+        except FileNotFoundError:
+            return 0
 
-    if choice == '1':
-        questions = question()
-        choices = quest_choices()
-        answers = answer()
+    question_num = starting_num() + 1
 
-        loading_animation()
+    # Input question
+    def question():
+        user_question = str(input(Fore.CYAN + "\nEnter your preferred question: "))
 
-        file.write(f"{question_num}. Question: {questions}\n")
+        return user_question
+
+    # Input choices
+    def quest_choices():
+        choices = {}
         for letter in ['A', 'B', 'C', 'D']:
-            file.write(f"{letter}. {choices[letter]}\n")
-        file.write(f"Correct Answer: {answers}\n\n")
+            choices[letter] = input(Fore.BLUE + f"Enter choice {letter} : ")
 
-        question_num += 1
+        return choices
 
-    elif choice == '7':
-        print(Fore.MAGENTA + "Exiting, have a nice day!\n")
-        file.close()
-        break
+    # Input correct answer
+    def answer():
+        user_ans = str(
+            input(Fore.LIGHTBLUE_EX + "\nEnter the correct answer (A,B,C,D) from provided question: ")).upper()
+        while user_ans not in ["A", "B", "C", "D"]:
+            user_ans = input(Fore.RED + "Invalid input. Please enter A, B, C, or D: ").upper()
+
+        return user_ans
+
+    # Loading text after questions, choices and answers are inputted
+    def loading_animation():
+        print(Fore.LIGHTRED_EX + "Saving your question", end='')
+        for _ in range(3):
+            sys.stdout.write(Fore.RED + ".")
+            sys.stdout.flush()
+            time.sleep(0.75)
+        print(Fore.YELLOW + "\nSaved successfully! Check your text file to see your inputted values.")
+        print()
+
+    while True:
+        print(Fore.LIGHTYELLOW_EX + "Enter your question!")
+        while True:
+            choice = str(input(Fore.YELLOW + "Enter '7' to exit. Enter '1' to continue: "))
+            if choice in ['1', '7']:
+                break
+
+            else:
+                print(Fore.RED + "Invalid input. Enter just number '1' to continue, and '7' to exit\n")
+
+        if choice == '1':
+            questions = question()
+            choices = quest_choices()
+            answers = answer()
+
+            loading_animation()
+
+            file.write(f"{question_num}. Question: {questions}\n")
+            for letter in ['A', 'B', 'C', 'D']:
+                file.write(f"{letter}. {choices[letter]}\n")
+            file.write(f"Correct Answer: {answers}\n\n")
+
+            question_num += 1
+
+        elif choice == '7':
+            print(Fore.MAGENTA + "Exiting, have a nice day!\n")
+            file.close()
+            break
 
 # ---- MAIN QUIZ ----
 file = 'quiz_txt.txt'
 
 quiz_questions = read_txt_file(file)
+
+compiler_quiz()
 execute_quiz(quiz_questions)
